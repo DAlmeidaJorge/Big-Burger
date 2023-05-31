25d84e21101b50007fbe04b0e38f1cfa93dce1b5
DAlmeidaJorge <duartejorge_93@hotmail.com>
2023-05-31 23:15:54 +0100

    started navbar

---
+++ big-burger/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+    <div className="logo">
+    <img src="https://static.vecteezy.com/system/resources/previews/017/503/957/original/hot-burgers-logo-illustration-modern-burgers-emblem-art-free-vector.jpg" alt="Burger-Logo" />
+    </div>
+    <div className="nav-links">
+      <a href="">What to eat today?</a>
+      <a href="">Promotions</a>
+      <a href="">Who we are?</a>
+      <a href="">Big n'Go!</a>
+      <a href="">Where are we?</a>
+      <a href="">Contacts</a>
+    </div>
+      
+    </div>
+    
+ 
+  
+  )
+}
+
+export default Navbar
